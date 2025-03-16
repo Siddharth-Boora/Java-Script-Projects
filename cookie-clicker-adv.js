cc2efb4baddd2f02c4b348a825a26e4f29926377
Siddharth-Boora <sidboora194@gmail.com>
2025-03-16 09:12:05 -0700

    Create cookie-clicker-adv.js

---
+++ cookie-clicker-adv.js
@@ -0,0 +1,39 @@
+p {
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+.cookieButton {
+  cursor: pointer;
+  border: none;
+  border-radius: 100px;
+  background-color: rgb(168, 109, 50);
+  transition: opacity 0.15s;
+  border: solid;
+}
+.cookieButton:hover {
+  cursor: pointer;
+  opacity: 0.8;
+}
+.cookieButton:active {
+  cursor: pointer;
+  opacity: 0.5;
+}
+.cookieClickUpgradeButton, .cookieACUpgradeButton {
+  font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  border: solid;
+  background-color: lightblue;
+  cursor: pointer;
+  transition: opacity 0.15s;
+  height: 80px;
+  border-radius: 10px;
+}
+.cookieClickUpgradeButton:hover, .cookieACUpgradeButton:hover {
+  cursor: pointer;
+  opacity: 0.8;
+}
+.cookieClickUpgradeButton:active, .cookieACUpgradeButton:active {
+  cursor: pointer;
+  opacity: 0.5;
+}
+.cookieClickUpgradeButtonTitle, .cookieACUpgradeButtonTitle {
+  font-weight: bold;
+}
